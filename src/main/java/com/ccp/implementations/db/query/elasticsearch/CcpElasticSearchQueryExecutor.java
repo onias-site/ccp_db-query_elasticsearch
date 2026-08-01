85f7981509df1ffe374ea9834db8dc8e034c7018
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-07-31 23:55:05 -0300

    refatoracao completa do login

--- src/main/java/com/ccp/implementations/db/query/elasticsearch/CcpElasticSearchQueryExecutor.java
+++ src/main/java/com/ccp/implementations/db/query/elasticsearch/CcpElasticSearchQueryExecutor.java
@@ -7,7 +7,7 @@ import com.ccp.especifications.db.query.CcpQueryExecutor;
  * Provedor de DI que expõe {@code ElasticSearchQueryExecutor} como implementação de {@code CcpQueryExecutor}.
  */
 public class CcpElasticSearchQueryExecutor implements CcpInstanceProvider<CcpQueryExecutor>  {
-	
+
 	public CcpQueryExecutor getInstance() {
 		return new ElasticSearchQueryExecutor();
 	}
